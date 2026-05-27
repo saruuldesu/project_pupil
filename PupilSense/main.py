a43bc116d6611555c104c93f0168e68621ca2795
saruuldesu <saruuldesu@icloud.com>
2026-05-28 02:03:55 +0900

    add vercel cors

--- PupilSense/main.py
+++ PupilSense/main.py
@@ -9,11 +9,16 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from make import main
 
 app = FastAPI()
-
-# Allow requests from your Next.js site
+@app.get("/")
+def root():
+    return {"status": "ok"}
+# Allow requests from local + Vercel frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://project-pupil-jowrju5jh-saruul-s-projects.vercel.app/",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
