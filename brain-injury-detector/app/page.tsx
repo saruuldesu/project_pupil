eec226c001c4b17d3b6c67b4d921bc2da84dec92
saruuldesu <saruuldesu@icloud.com>
2026-05-28 02:13:46 +0900

    connect frontend to render backend

--- brain-injury-detector/app/page.tsx
+++ brain-injury-detector/app/page.tsx
@@ -41,7 +41,9 @@ export default function Home() {
     formData.append("rightImage", rightImage);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+
+      const response = await fetch(`${API_URL}/predict`, {
         method: "POST",
         body: formData,
       });
